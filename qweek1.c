52fcc987d670b2cb8cc30b5725e3ced4235dd7cc
Sskkec <41425538+Sskkec@users.noreply.github.com>
2018-07-19 15:12:10 +0530

    Update qweek1.c

--- qweek1.c
+++ qweek1.c
@@ -1,6 +1,7 @@
 //bgfbdhgdnhyj
 //kilkjli
- prize Testyourskill (All_Languages) {
+ prize Testyourskill (All_Languages)
+ {
  char Venue[7]= "auditorium" ;
 char Date[10]= "21 st july 2018" ;
 char Time[10]= "9:00 AM" ;
