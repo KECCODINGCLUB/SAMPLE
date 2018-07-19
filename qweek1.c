6b077bc109ac945d12bbabe361bb872d6aa97af2
Rockersdream <41425478+Rockersdream@users.noreply.github.com>
2018-07-19 15:23:48 +0530

    Update qweek1.c

--- qweek1.c
+++ qweek1.c
@@ -1,14 +1,17 @@
-//bgfbdhgdnhyj
-//kilkjli
- prize Testyourskill (All_Languages) {
+#include<stdio.h>
+#include<conio.h>
+ prize Testyourskill (All_Languages)
+ {
  char Venue[7]= "auditorium" ;
-char Date[10]= "21 st july 2018" ;
-char Time[10]= "9:00 AM" ;
+char Date[10]= "19 th july 2018" ;
+char Time[10]= "3:21 pm" ;
 return winprize; 
 }
-main ( ){
+main ( )
+{
  int brain,passion;
 scanf(" %d%d", &brain,&passion);
 if (brain && passion) {   
            TestYourskill
+}
 
