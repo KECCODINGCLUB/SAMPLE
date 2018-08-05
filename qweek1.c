a4486e3542c77c009aec753298e2ece96cd67b40
nkcharan007 <40414538+nkcharan007@users.noreply.github.com>
2018-08-05 10:52:10 +0530

    third week challenge
    third week challenge

--- qweek1.c
+++ qweek1.c
@@ -1,14 +1,49 @@
-//bgfbdhgdnhyj
-//kilkjli
- prize Testyourskill (All_Languages) {
- char Venue[7]= "auditorium" ;
-char Date[10]= "21 st july 2018" ;
-char Time[10]= "9:00 AM" ;
-return winprize; 
+/**
+
+// @Kuppam Engineering College 
+
+// @KEC Coding Club
+
+*/
+
+/*
+
+//Third Week Challenge.
+
+*/
+
+/**
+
+ * @Authors {
+
+ N.K.CHARAN , M.AYATHULLAH 
+
+};
+
+ * Team_Name: #Riots-18
+
+*/
+#include<stdio.h>
+
+#include<conio.h>
+
+void main() {
+
+  int k, n, w;
+
+  int total;
+
+  clrscr();
+
+	 printf("\n Enter the values of Oranges:");
+
+	    scanf("%d %d %d", &k, &n, &w);		// Scans the values
+
+  total = k * w * (w + 1) / 2;                        // Checks the conditions
+
+  printf("The Borrowed Amount from her friend is is \n %d", (total - n));      //Displays the values
+
+  getch();
+
 }
-main ( ){
- int brain,passion;
-scanf(" %d%d", &brain,&passion);
-if (brain && passion) {   
-           TestYourskill
 
